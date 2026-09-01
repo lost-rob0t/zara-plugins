1094278dfaf5e82f09bfb2a25102d102d722c9ee
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-09-01 16:50:45 -0400

    fix: keep client package stdlib-only

--- plugins/zara-starintel-server/lib/zara_starintel_server/__init__.py
+++ plugins/zara-starintel-server/lib/zara_starintel_server/__init__.py
@@ -2,15 +2,11 @@
 
 from .client import StarIntelClient, StarIntelError
 from .config import StarIntelConfig, StarIntelConfigError
-from .plugin import PLUGIN_VERSION, ZaraStarIntelServerPlugin, create_plugin
 
 
 __all__ = [
-    "PLUGIN_VERSION",
     "StarIntelClient",
     "StarIntelConfig",
     "StarIntelConfigError",
     "StarIntelError",
-    "ZaraStarIntelServerPlugin",
-    "create_plugin",
 ]
