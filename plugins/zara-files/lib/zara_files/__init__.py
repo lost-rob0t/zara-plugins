1a6647ad7353daf9a2e1dcf59141510139949c15
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-09-05 04:05:08 -0400

    fix(files): keep domain imports stdlib-only

--- plugins/zara-files/lib/zara_files/__init__.py
+++ plugins/zara-files/lib/zara_files/__init__.py
@@ -1,4 +1,3 @@
 from .files import FileDomain, FileDomainError
-from .plugin import ZaraFilesPlugin, create_plugin
 
-__all__ = ["FileDomain", "FileDomainError", "ZaraFilesPlugin", "create_plugin"]
+__all__ = ["FileDomain", "FileDomainError"]
