bb3d28f7212cffb2e42a6ff181159533344ae30c
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-09-05 04:04:59 -0400

    fix(browser): keep domain imports stdlib-only

--- plugins/zara-browser/lib/zara_browser/__init__.py
+++ plugins/zara-browser/lib/zara_browser/__init__.py
@@ -1,11 +1,8 @@
 from .browser import BrowserError, BrowserSession, FakeBrowserBackend, UnavailableBrowserBackend
-from .plugin import ZaraBrowserPlugin, create_plugin
 
 __all__ = [
     "BrowserError",
     "BrowserSession",
     "FakeBrowserBackend",
     "UnavailableBrowserBackend",
-    "ZaraBrowserPlugin",
-    "create_plugin",
 ]
