13e9afc651bf33029b2161490a14308c99e10419
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-09-05 03:24:14 -0400

    fix(github): define discovery factory for registry

--- plugins/zara-github/zara-plugin/zara_github.py
+++ plugins/zara-github/zara-plugin/zara_github.py
@@ -1,5 +1,7 @@
 """Zara discovery entry for the GitHub service plugin."""
 
-from zara_github.plugin import create_plugin
 
-__all__ = ["create_plugin"]
+def create_plugin():
+    from zara_github.plugin import create_plugin as create_service
+
+    return create_service()
