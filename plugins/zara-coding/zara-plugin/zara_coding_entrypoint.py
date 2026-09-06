68ec5c1f1b91c6da3e36558a0d678ff09465ece6
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-09-06 06:54:33 -0400

    feat: expose task-state coding service entrypoint

--- plugins/zara-coding/zara-plugin/zara_coding_entrypoint.py
+++ plugins/zara-coding/zara-plugin/zara_coding_entrypoint.py
@@ -4,6 +4,6 @@ PLUGIN_VERSION = "0.1.0"
 
 
 def create_plugin():
-    from zara_coding.plugin import create_plugin as create_service
+    from zara_coding.task_plugin import create_plugin as create_service
 
     return create_service()
