e7b0e273480c9497317581b3c8dd3f687e0e44f4
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-09-17 02:42:35 -0400

    feat(calendar): export provider backends

--- plugins/zara-calendar/lib/zara_calendar/__init__.py
+++ plugins/zara-calendar/lib/zara_calendar/__init__.py
@@ -1,3 +1,12 @@
+from .caldav import CalDavCalendarBackend, CalDavCalendarError
 from .domain import CalendarDomain, CalendarError
+from .google import GoogleCalendarBackend, GoogleCalendarError
 
-__all__ = ["CalendarDomain", "CalendarError"]
+__all__ = [
+    "CalDavCalendarBackend",
+    "CalDavCalendarError",
+    "CalendarDomain",
+    "CalendarError",
+    "GoogleCalendarBackend",
+    "GoogleCalendarError",
+]
