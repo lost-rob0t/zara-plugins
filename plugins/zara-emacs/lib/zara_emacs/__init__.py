85047117a8f7e91f06bcfebd5175b74860387a2c
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-09-20 09:40:11 -0400

    fix(zara-emacs): keep expert adapter import explicit

--- plugins/zara-emacs/lib/zara_emacs/__init__.py
+++ plugins/zara-emacs/lib/zara_emacs/__init__.py
@@ -2,24 +2,10 @@
 
 from .client import EmacsClient, EmacsError
 from .config import EmacsConfig, EmacsConfigError
-from .expert import (
-    EMACS_EXPERT_ID,
-    EMACS_EXPERT_OPERATIONS,
-    EMACS_EXPERT_SOURCE,
-    EmacsExpertAdapter,
-    EmacsExpertAdapterError,
-    validate_emacs_descriptor,
-)
 
 __all__ = [
-    "EMACS_EXPERT_ID",
-    "EMACS_EXPERT_OPERATIONS",
-    "EMACS_EXPERT_SOURCE",
     "EmacsClient",
     "EmacsError",
     "EmacsConfig",
     "EmacsConfigError",
-    "EmacsExpertAdapter",
-    "EmacsExpertAdapterError",
-    "validate_emacs_descriptor",
 ]
