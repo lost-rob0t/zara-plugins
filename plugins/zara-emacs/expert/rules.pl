4cd6d972f6d694c0e68f33bb1eab72e166b264e3
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-09-19 21:16:32 -0400

    zara-emacs: add Nix-built Emacs Prolog expert source

---
+++ plugins/zara-emacs/expert/rules.pl
@@ -0,0 +1,29 @@
+:- module(emacs_expert, [
+    emacs_command/1,
+    emacs_function/1,
+    emacs_variable/1,
+    emacs_documentation/3,
+    emacs_keys/2,
+    emacs_search/3
+]).
+
+emacs_command(Symbol) :-
+    emacs_symbol_kind(Symbol, command).
+
+emacs_function(Symbol) :-
+    emacs_symbol_kind(Symbol, function).
+
+emacs_variable(Symbol) :-
+    emacs_symbol_kind(Symbol, variable).
+
+emacs_documentation(Symbol, Kind, Doc) :-
+    emacs_doc(Symbol, Kind, Doc).
+
+emacs_keys(Symbol, Keys) :-
+    findall(Key, emacs_keybinding(Symbol, Key), Keys).
+
+emacs_search(Needle, Symbol, Kind) :-
+    downcase_atom(Needle, LowerNeedle),
+    emacs_doc(Symbol, Kind, Doc),
+    downcase_atom(Doc, LowerDoc),
+    sub_atom(LowerDoc, _, _, _, LowerNeedle).
