e8980d5357fae38e2d8e392ed072d1e85bd71696
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-09-19 21:16:46 -0400

    zara-emacs: add reproducible Emacs expert package

--- flake.nix
+++ flake.nix
@@ -127,6 +127,24 @@
               };
             };
 
+          emacsExpert = pkgs.runCommand "zara-emacs-expert"
+            {
+              nativeBuildInputs = [ pkgs.emacs-nox pkgs.coreutils ];
+              src = self;
+            }
+            ''
+              mkdir -p $out/share/zara/experts/emacs
+              export OUT=$out/share/zara/experts/emacs/emacs-kb.pl
+              ${pkgs.emacs-nox}/bin/emacs --batch -Q \
+                -l $src/plugins/zara-emacs/expert/export-emacs-kb.el \
+                --eval '(zara-emacs-export-kb (getenv "OUT"))'
+              cp $src/plugins/zara-emacs/expert/rules.pl \
+                $out/share/zara/experts/emacs/rules.pl
+              test -s $out/share/zara/experts/emacs/emacs-kb.pl
+              grep -q '^emacs_build_version(' $out/share/zara/experts/emacs/emacs-kb.pl
+              grep -q '^emacs_kb_complete(' $out/share/zara/experts/emacs/emacs-kb.pl
+            '';
+
           pluginPackages = pkgs.lib.listToAttrs (
             map (entry: pkgs.lib.nameValuePair entry.name (mkPluginPackage entry)) plugins
           );
@@ -234,6 +252,7 @@
         in
         {
           packages = pluginPackages // {
+            zara-emacs-expert = emacsExpert;
             zara-plugins = pluginEnv;
             default = pluginEnv;
           };
