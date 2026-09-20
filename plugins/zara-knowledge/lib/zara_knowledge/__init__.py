5b15d8a16559dbe5d9c34f509d2aa25c08d9357f
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-09-19 22:46:48 -0400

    chore(zara-knowledge): export wiki gate API

--- plugins/zara-knowledge/lib/zara_knowledge/__init__.py
+++ plugins/zara-knowledge/lib/zara_knowledge/__init__.py
@@ -1,6 +1,20 @@
-"""Sourced knowledge providers for Zara."""
+"""Sourced web and wiki knowledge providers for Zara."""
 
 from .brave import BraveProvider, BraveProviderError
 from .core import KnowledgeEngine, SourcedResult
+from .store import LocalWikiProvider, WikiStore
+from .wiki import GateCatalog, GateSpec, MediaWikiGate, WikiGateError, WikiManager
 
-__all__ = ["BraveProvider", "BraveProviderError", "KnowledgeEngine", "SourcedResult"]
+__all__ = [
+    "BraveProvider",
+    "BraveProviderError",
+    "GateCatalog",
+    "GateSpec",
+    "KnowledgeEngine",
+    "LocalWikiProvider",
+    "MediaWikiGate",
+    "SourcedResult",
+    "WikiGateError",
+    "WikiManager",
+    "WikiStore",
+]
