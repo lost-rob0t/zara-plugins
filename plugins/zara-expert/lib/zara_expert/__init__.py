47988a4f3d3f6fab4dd57d53246ab490f4605481
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-09-19 23:27:10 -0400

    feat(zara-expert): export composition adapters

--- plugins/zara-expert/lib/zara_expert/__init__.py
+++ plugins/zara-expert/lib/zara_expert/__init__.py
@@ -1,3 +1,34 @@
+from .composition import (
+    CompositionError,
+    DelegationRequest,
+    EffectiveStyle,
+    EvidenceNode,
+    ExpertCatalogAdapter,
+    HostExpertInvoker,
+    InvocationFence,
+    InvocationResult,
+    MetaExpertComposer,
+    SharedSymbolicBudget,
+    StyleOverlay,
+    StyleScope,
+    resolve_style,
+)
 from .domain import ExpertError, ExpertHost
 
-__all__ = ["ExpertError", "ExpertHost"]
+__all__ = [
+    "CompositionError",
+    "DelegationRequest",
+    "EffectiveStyle",
+    "EvidenceNode",
+    "ExpertCatalogAdapter",
+    "ExpertError",
+    "ExpertHost",
+    "HostExpertInvoker",
+    "InvocationFence",
+    "InvocationResult",
+    "MetaExpertComposer",
+    "SharedSymbolicBudget",
+    "StyleOverlay",
+    "StyleScope",
+    "resolve_style",
+]
