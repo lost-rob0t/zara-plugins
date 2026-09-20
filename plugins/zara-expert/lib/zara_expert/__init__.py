19e43eebce36c85d2724fd772ff2b74679c3bc4f
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-09-19 23:21:22 -0400

    feat(zara-expert): export language adapter API

--- plugins/zara-expert/lib/zara_expert/__init__.py
+++ plugins/zara-expert/lib/zara_expert/__init__.py
@@ -1,3 +1,22 @@
 from .domain import ExpertError, ExpertHost
+from .language_adapters import (
+    NIM_EXPERT,
+    PROLOG_EXPERT,
+    PYTHON_EXPERT,
+    LanguageExpertAdapter,
+    LanguageExpertProfile,
+    language_expert_schemas,
+    profile_for_language,
+)
 
-__all__ = ["ExpertError", "ExpertHost"]
+__all__ = [
+    "ExpertError",
+    "ExpertHost",
+    "LanguageExpertAdapter",
+    "LanguageExpertProfile",
+    "PROLOG_EXPERT",
+    "PYTHON_EXPERT",
+    "NIM_EXPERT",
+    "language_expert_schemas",
+    "profile_for_language",
+]
