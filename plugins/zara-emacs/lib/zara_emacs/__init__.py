e1330768bc76ca2737a65254a284d2b31b56c76b
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-09-20 08:31:51 -0400

    feat(zara-emacs): export EmacsExpert adapter

--- plugins/zara-emacs/lib/zara_emacs/__init__.py
+++ plugins/zara-emacs/lib/zara_emacs/__init__.py
@@ -2,5 +2,24 @@
 
 from .client import EmacsClient, EmacsError
 from .config import EmacsConfig, EmacsConfigError
+from .expert import (
+    EMACS_EXPERT_ID,
+    EMACS_EXPERT_OPERATIONS,
+    EMACS_EXPERT_SOURCE,
+    EmacsExpertAdapter,
+    EmacsExpertAdapterError,
+    validate_emacs_descriptor,
+)
 
-__all__ = ["EmacsClient", "EmacsError", "EmacsConfig", "EmacsConfigError"]
+__all__ = [
+    "EMACS_EXPERT_ID",
+    "EMACS_EXPERT_OPERATIONS",
+    "EMACS_EXPERT_SOURCE",
+    "EmacsClient",
+    "EmacsError",
+    "EmacsConfig",
+    "EmacsConfigError",
+    "EmacsExpertAdapter",
+    "EmacsExpertAdapterError",
+    "validate_emacs_descriptor",
+]
