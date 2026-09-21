56d1ab5a88a2f47d81812807a7169b4d0f79b455
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-09-21 00:38:42 -0400

    feat(doordash): export plugin domain

---
+++ plugins/zara-doordash/lib/zara_doordash/__init__.py
@@ -0,0 +1,4 @@
+from .domain import DoorDashDomain, DoorDashError
+from .plugin import ZaraDoorDashPlugin, create_plugin
+
+__all__ = ["DoorDashDomain", "DoorDashError", "ZaraDoorDashPlugin", "create_plugin"]
